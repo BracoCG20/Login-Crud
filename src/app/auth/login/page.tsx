bf02356bf464474edf9f49febfbc89af24a4c6eb
Cristian Braco <braco1209crist@gmail.com>
2025-07-08 10:33:48 -0500

    first commit

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,15 @@
+import { BookmarkIcon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+
+function LoginPage() {
+  return (
+    <div>
+      LoginPage
+      <Button size="2" color="jade">
+        <BookmarkIcon width="20" height="20" /> Bookmark
+      </Button>
+    </div>
+  );
+}
+
+export default LoginPage;
